7dbeed29c7b7eda461756eb37419574924a59908
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2022-03-17 15:06:01 +0000

    Update CP2_SIRS.py

--- CP2_SIRS.py
+++ CP2_SIRS.py
@@ -1,29 +1,25 @@
-#%%
 import matplotlib
 matplotlib.use('TKAgg')
 import math
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import scipy
-from sklearn.utils import resample
+
+import numpy as np
 from numpy.random import rand
 import random
 from numpy import savetxt
-import argparse
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import scipy
+from numpy import array
 
 from tqdm import tqdm
 
 #####################################################################################################
 #####################################################################################################
 #####################################################################################################
-
-parser = argparse.ArgumentParser(description="Game of Life simulation.")
-
-#state = str(input('Choose between a random (r) initial condition and one in a set of selected (s) initial conditions'))
-
-# make results reproducible
-#np.random.seed(10)
 
 
 # setting up the values for the grid
@@ -32,218 +28,260 @@ I = 2
 R = 0
 vals = [S, I, R]
 
-#%%
-# Command line args are in sys.argv[1], sys.argv[2] ..
-# sys.argv[0] is the script name itself and can be ignored
-# parse arguments
-# example: python3 MVP_CP2.py --grid-size 100 --interval 10 --random
+#(i) an absorbing state with all sites susceptible to the infection; 
+#       ---> 0.5, 0.6, 0.1
+#(ii) a dynamic equilibrium between S, I and R;
+#       ---> 0.5, 0.5, 0.5
+#(iii) a case with cyclic wave of infections through the lattice
+#       ---> 0.8, 0.1, 0.01
 
-# add arguments
-parser.add_argument('--sys-size', dest='N', required=False)
-parser.add_argument('--mov-file', dest='movfile', required=False)
-parser.add_argument('--interval', dest='interval', required=False)
-parser.add_argument('--p1', action='store_true', required=False)
-parser.add_argument('--p2', action='store_true', required=False)
-parser.add_argument('--p3', action='store_true', required=False)
-
-args = parser.parse_args()
+# pseudo-random numbers to use in future functions
+N = int(input('Array size: '))
 
 #####################################################################################################
 #####################################################################################################
 #####################################################################################################
-N = 50
 
 
-
-def random_state(N):
+def random_state():
     '''
     returns a grid of NxN random values
     '''
-    return np.random.choice(vals, size=(N, N))
+    return np.random.choice(vals, size=(N,N))
     
     
 
-def selection(p1, p2, p3):#frameNum, img, N, p1, p2, p3):
+def selection(grid, p1, p2, p3):
     '''
-    S--> I with probability p1 if at least one neighbout is I (i.e., if sum of neighbours is greater than 8)
+    S--> I with probability p1 if at least one neighbout is I
     I--> R with probability p2
     R--> S with probability p3
     '''
-    life = 0
-    grid = random_state(N)
-    new_grid = grid.copy()
     
-    print(new_grid[np.where(new_grid == I)])
+    r = rand()
     
-    S_points = 
-    I_points = 
-    R_points = 
+    i, j = random.choices(np.arange(N), k=2)          # choose 1 random cell
     
-    (new_grid[np.where(new_grid == I)])[np.random.rand(*new_grid.shape) < p2] = R
-    (new_grid[np.where(new_grid == R)])[np.random.rand(*new_grid.shape) < p1] = S
+    times_updated = 0
     
-    
-    # img.set_data(new_grid)
-    # grid[:] = new_grid[:]
-    # return img,
+    if grid[i ,j] == S:
+        # if life_array[i,j] >= I :
+        if (grid[(i+1)%N,j] == I or grid[(i-1)%N,j] == I or grid[i,(j-1)%N] == I or grid[i,(j+1)%N] == I):
+            if p1 > r:
+                grid[i, j] = I
+    elif grid[i ,j] == I:
+        if p2 > r:
+                grid[i, j] = R
+    elif grid[i ,j] == R:
+        if p3 > r:
+                grid[i, j] = S
 
-selection(0.5, 0.4, 0.1)
+    return grid
 
-# def move(grid, N):
-#     '''
-#     S--> I with probability p1 if at least one neighbout is I (i.e., if sum of neighbours is greater than 8)
-#     I--> R with probability p2
-#     R--> S with probability p3
-#     '''
-#     life = 0
-#     new_grid = grid.copy()
-#     # loop through the lattice to determine living neighbours 
-#     for i in range(N):
-#         for j in range(N):
-#             # compute 8-neighbor sum
-#             life = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-#                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
-#                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
-#                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/ON)
-            
-#             if grid[i,j] == ON:     # if cell is live
-#                 if (life < 2) or (life > 3):
-#                     new_grid[i,j] = OFF
-                
-#             else:       # if cell is dead
-#                 if life == 3:
-#                     new_grid[i,j] = ON
 
-#     grid[:] = new_grid[:]
-#     return grid,
+
 
     
 
-# def simulation():
+def simulation():
 
-#     # set grid size
-#     N = 100
-#     if args.N and int(args.N) > 8:
-#         N = int(args.N)
-         
-#     # set animation update interval
-#     updateInterval = 10
-#     if args.interval:
-#         updateInterval = int(args.interval)
- 
-#     # declare grid
-#     grid = np.array([])
- 
-#     # check if "glider" demo flag is specified
-#     if args.glider:
-#         grid = np.zeros(N*N).reshape(N, N)
-#         glider(1, 1, grid)
-
-#     elif args.oscillator:
-#         grid = np.zeros(N*N).reshape(N, N)
-#         oscillator(int(N/2), int(N/2), grid)
- 
-#     elif args.random:   # populate grid with random on/off -
-#             # more off than on
-#         grid = random_state(N)
-
-#     # set up animation
-#     fig, ax = plt.subplots()
-#     img = ax.imshow(grid, interpolation='nearest')
-#     ani = animation.FuncAnimation(fig, selection, fargs=(img, grid, N, ),
-#                                   frames = 10,
-#                                   interval=updateInterval,
-#                                   save_count=50)
- 
-#     # # of frames?
-#     # set output file
-#     if args.movfile:
-#         ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
- 
-#     plt.show()
-
-
-
-
-# def equil():
-    
-#     # set grid size
-#     N = 100
-
-#     # set number of simulations
-#     N_sim = 100
-
-#     # number of steps
-#     nstep = 10000
-
-#     # set number of 
-    
-#     t_equil = []
-    
-#     for i in tqdm(range(N_sim)):
-#         active_sites = []
-#         init_grid = random_state(N)
+    '''
+    Animated simulation of the SIRS model
         
-#         for n in range(nstep):
-#             new_grid = move(init_grid, N)
-#             active_sites.append(np.sum(new_grid))
-            
-#             #if n >= 1000:
-#             if n>100 and np.all((active_sites[n-50:n] == active_sites[n])) == True:
-#                 t_equil.append(n)
-#                 break
-#     np.savetxt('equiltimes.csv', t_equil, delimiter=',')
-                
-#     return t_equil
-
-# def plot_equil():
-#     t_equil = equil()
-#     np.savetxt('equiltimes.csv', t_equil, delimiter=',')
-#     plt.hist(t_equil, 40, density=True)
-#     plt.xlabel('Times needed to equilibrate')
-#     plt.ylabel('Normalised Frequency')
-#     plt.savefig('equil.png')
-#     plt.show()
-    
-# #%%
-# def centre_of_mass():
-#     # set grid size
-#     N = 100
-
-#     # number of steps
-#     nstep = 100
-
-#     grid = np.zeros((N,N))
-#     init_grid = glider(1, 1, grid)
-#     #print(init_grid)
-    
-#     nonZeroMasses = np.nonzero(init_grid)
-#     com = np.array((np.average(nonZeroMasses[0]), np.average(nonZeroMasses[1])))
-#     com_array = []
-#     com_array.append(com.tolist())
-    
-#     time = 0
-#     times = []
-#     times.append(time)
-    
-#     for i in range(N*2):
-#         time += 1
-#         times.append(0)
-#         new_grid = move(init_grid, N)
-#         nonZeroMasses_new = np.nonzero(new_grid)[1:3]
+    Returns
+    -------
+    animated simulation
         
-#         #print(nonZeroMasses_new)
-#         com = np.array((np.average(nonZeroMasses_new[0]), np.average(nonZeroMasses_new[1])))
-#         com_array.append(com.tolist())
-#         #delta_com = (com_array[i-1]%N - com_array[i]%N)
-#     print(np.array(com_array))
+    '''
     
+    behaviour = str(input("Choose behaviour: \n 'A' : an absorbing state with all sites susceptible to the infection; \n 'D' : a dynamic equilibrium between S, I and R; \n 'C' : a case with cyclic wave of infections through the lattice \n"))
     
+    if behaviour == 'A':
+        p1, p2, p3 = 0.5, 0.6, 0.1
+        
+    elif behaviour == 'D':
+        p1, p2, p3 = 0.5, 0.5, 0.5
+        
+    elif behaviour == 'C':
+        p1, p2, p3 = 0.8, 0.1, 0.01
+    
+    else:
+        print("Invalid behaviour chosen. Please run again")
+    
+    nstep = 1000
+
+    #initialise spins randomly
+    state = random_state()
+
+    fig = plt.figure()
+    im = plt.imshow(state, animated=True)
+    
+    for n in tqdm(range(nstep)):
+        for i in range(N**2):
+            state = selection(state, p1, p2, p3)
+            
+        #occasionally plot or update measurements, eg every 10 sweeps
+        #if(n%10==0):
+        plt.cla()
+        plt.title(n)
+        im = plt.imshow(state, animated=True)
+        plt.draw()
+        plt.pause(0.0001)
+
 
 #%%
+#%%
 
-#  # call main
-# if __name__ == '__main__':
-#     # simulation()
-#     # equil()
-#     # plot_equil()
+def phase(start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps):
+    
+    nstep = sweeps + 101
+    p1_array = np.linspace(start_p1, end_p1, no_points1)
+    p3_array = np.linspace(start_p3, end_p3, no_points3)
+    
+    probs = [(p1, p3) for p1 in p1_array for p3 in p3_array]
+    print(len(probs))
+    #initialise spins randomly
+        
+    avg_infected = []
+    I_var = []
+    I_errs = []
+    
+    for comb in tqdm(range(len(probs))):
+        p1, p3 = probs[comb]
+        state = random_state()
+        no_infected = []
+        
+        for n in tqdm(range(nstep)):
+            
+            for i in range(N**2):
+                state = selection(state, p1, 0.5, p3)
+                
+            if n > 100:
+                # count number of infected sites
+                no_infected.append(np.count_nonzero(state==I))
+
+        # estimate bootstrap errors for the case where we have one of p1 or p3 fixed
+        if (len(p1_array) == 1 or len(p3_array) == 1):
+            resampled_calc = []
+            for i in range(200):
+                resampled_I = np.random.choice(no_infected, 800)
+                new_calc_I = np.var(resampled_I)
+                resampled_calc.append(new_calc_I)
+            
+            I_errs.append(np.var(resampled_calc)**(1/2))
+            
+        avg_infected.append(np.mean(no_infected))
+        I_var.append(np.var(no_infected))
+    
+    avg_infected = np.array(avg_infected)/N**2
+
+    print(len(I_errs))
+    
+    np.savetxt(f'infected_{N}.csv', avg_infected, delimiter=',')
+    np.savetxt(f'variance_{N}.csv', np.array(I_var), delimiter=',')
+    np.savetxt(f'errors_{N}.csv', np.array(I_errs), delimiter=',')
+        
+    return avg_infected, np.array(I_var), p1_array, p3_array, np.array(I_errs)
+
+
+def plot_inf():
+    start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps = 0., 0., 1., 1., 21, 21, 1000
+    
+    avg_infected, I_var, p1_array, p3_array, _ = phase(start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps)
+    xx, yy = np.meshgrid(p1_array, p3_array)
+
+    fig = plt.figure()
+    ax1 = plt.contourf(xx,yy,avg_infected.reshape((len(p1_array),len(p3_array))))
+    
+    plt.savefig(f'phase_plot_{N}.png')
+    plt.show()
+
+
+
+def plot_var():
+    start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps = 0., 0., 1., 1., 21, 21, 1000
+    
+    avg_infected, I_var, p1_array, p3_array, _ = phase(start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps)
+    xx, yy = np.meshgrid(p1_array, p3_array)
+
+    fig = plt.figure()
+    ax1 = plt.contourf(xx,yy,I_var.reshape((len(p1_array),len(p3_array))))
+    
+    plt.savefig(f'var_plot_{N}.png')
+    plt.show()
+
+
+
+def trimmed_plot():
+    start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps = 0.2, 0.5, 0.5, 0.5, 21, 1, 10000
+    
+    avg_infected, I_var, p1_array, p3_array, I_errs = phase(start_p1, start_p3, end_p1, end_p3, no_points1, no_points3, sweeps)
+    plt.plot(p1_array, I_var)
+    plt.errorbar(p1_array, I_var, yerr=I_errs, fmt='x', ecolor = 'lightblue',color='m')
+    plt.xlabel('p1')
+    plt.ylabel('Variance of the number of infected sites')
+    plt.savefig(f'trimmed_var_plot_{N}.png')
+    plt.show()
+    
+    
+def fIm_plot(sweeps):
+    # modify our program so that some fraction of agents are permanently immune to the infection (i.e., in the R state)
+    # loop over fIm array
+    fIm_array = np.arange(0., 1.1, 0.1)
+    
+    avg_infected = []
+    I_errs = []
+        
+    for frac in tqdm(fIm_array):
+        avg_infected_single = []
+
+        for i in tqdm(range(5)):
+            # choose random state only with S and I and then turn fraction of them to unaffected 
+            state = random_state()
+            indices_i = np.random.randint(0, state.shape[0], int(state.size*frac))
+            indices_j = np.random.randint(0, state.shape[0], int(state.size*frac))
+            state[indices_i, indices_j] = 3.
+
+            nstep = sweeps + 101
+                    
+            no_infected = []
+            
+            for n in tqdm(range(nstep)):
+                
+                for i in range(N**2):
+                    state = selection(state, 0.5, 0.5, 0.5)
+                    
+                if n > 100:
+                    # count number of infected sites
+                    no_infected.append(np.count_nonzero(state==I))
+            avg_infected_single.append(np.mean(no_infected))
+        
+        # estimate errors using the standard error for the mean
+        I_errs.append(np.var(no_infected)/np.sqrt(5))
+        avg_infected.append(np.mean(avg_infected_single))
+        
+    avg_infected = np.array(avg_infected)/N**2
+
+    
+    np.savetxt(f'infected_{N}_fIm.csv', avg_infected, delimiter=',')
+    np.savetxt(f'fIm_array{N}_fIm.csv', fIm_array, delimiter=',')
+    np.savetxt(f'errors_{N}_fIm.csv', np.array(I_errs), delimiter=',')
+
+    plt.plot(fIm_array, avg_infected)
+    plt.errorbar(fIm_array, avg_infected, yerr=I_errs, fmt='x', ecolor = 'lightblue',color='m')
+    plt.xlabel('fIm')
+    plt.ylabel('Average number of infected sites')
+    plt.show()
+    
+    
+
+
+ # call main
+if __name__ == '__main__':
+    # move(random_state(50), 50)
+    # simulation()
+    # phase()
+    # plot_inf()
+    # plot_var()
+    # trimmed_plot()
+    fIm_plot(10000)
