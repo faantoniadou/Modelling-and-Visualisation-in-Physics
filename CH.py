ccacc2cbb586dbb01bca6e69b1ed89336247b269
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2022-03-30 16:59:15 +0100

    Add files via upload

---
+++ CH.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from os import system
+import sys
+import matplotlib
+matplotlib.use('TKAgg')
+from tqdm import tqdm 
+from matplotlib import animation
+
+
+a = 0.1
+M = 0.1
+k = 0.1
+phi0 = 0.
+
+dt = 2.
+dx = 1.
+
+
+def physical_sys(N):
+    # some random noise
+    grid = np.random.uniform(-0.1 + phi0, 0.1 + phi0, (N, N) )
+    
+    return grid
+
+
+def update(grid):     #frameNum, img, grid):
+    
+    mu = -a * grid + a * grid ** 3 - (k / dx**2) * ( np.roll(grid, 1, axis=0) + np.roll(grid, -1, axis=0) + 
+                                                     np.roll(grid, 1, axis=1) + np.roll(grid, -1, axis=1) - 4 * grid )
+    
+    grid = M * (dt / dx**2) * (np.roll(mu, 1, axis=0) + np.roll(mu, -1, axis=0) + 
+                               np.roll(mu, 1, axis=1) + np.roll(mu, -1, axis=1) - 4 * mu)
+
+    # img.set_data(grid)
+    
+    return grid
+
+
+
+def free_energy_density(grid):
+    return np.sum(-(a/2) * grid ** 2 + (a/4) * grid ** 4 + k / (2 * dx ** 2) \
+        * (np.roll(grid, 1, axis=0) - grid) ** 2 + (np.roll(grid, 1, axis=1) - grid) ** 2 )
+                  
+
+
+def simulation(animate=False):
+    N = 100
+    grid = physical_sys(N)
+    nstep = int(1e+6)+1
+    
+    plt.imshow(grid, vmax=1, vmin=-1, animated=True, cmap='plasma') 
+    plt.colorbar()
+    free_energies = np.empty(int(nstep/100)+1)
+
+    for i, step in tqdm(enumerate(range(nstep))):
+        grid += update(grid)
+        
+        if i%10 == 0 and animate==True:
+
+            plt.cla()   
+            plt.title(step)
+            im = plt.imshow(grid, vmax=1, vmin=-1, animated=True, interpolation='bilinear', cmap='plasma')
+            plt.draw()
+            plt.pause(0.0001)
+            
+        if i%100 == 0:
+            free_energies[int(i/100)] = free_energy_density(grid)
+            
+    np.savetxt('free_energies.csv', free_energies, delimiter=',')
+    
+
+def plot_energy():
+    x = np.arange(0,int(1e+6)+100,100)
+    energies = np.genfromtxt("free_energies.csv")
+
+    plt.xlabel('Time step')
+    plt.ylabel('Free Energy Density')
+    plt.title('Free energy density over time')
+
+    plt.xlim(0,np.max(x))
+    plt.plot(x, energies)
+    plt.show()
+
+
+
+# call main
+if __name__ == '__main__':
+    # simulation(animate=True)
+    plot_energy()
